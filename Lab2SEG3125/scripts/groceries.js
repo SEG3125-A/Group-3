de3da27671f5e5bc841004c1fd013056af30fb28
MichaelThompson <mthom242@uottawa.ca>
2024-01-24 14:46:35 -0500

    Added isOrganic and wrote the worst code of my life

--- Lab2SEG3125/scripts/groceries.js
+++ Lab2SEG3125/scripts/groceries.js
@@ -5,93 +5,113 @@ var products = [
         name: 'Carrot',
         isVegetarian: true,
         isGlutenFree: true,
+        isOrganic: true,
         price: 2.99
     },
     {
         name: 'Green Pepper',
         isVegetarian: true,
         isGlutenFree: true,
+        isOrganic: true,
         price: 2.99
     },
     {
         name: 'Cucumber',
         isVegetarian: true,
         isGlutenFree: true,
+        isOrganic: true,
         price: 3.99
     },
     {
         name: 'Bread',
         isVegetarian: true,
         isGlutenFree: false,
+        isOrganic: true,
         price: 4.99
     },
     {
         name: 'Crackers',
         isVegetarian: true,
         isGlutenFree: false,
+        isOrganic: false,
         price: 5.99
     },
     {
         name: 'Rice',
         isVegetarian: true,
         isGlutenFree: false,
+        isOrganic: false,
         price: 4.99
     },
     {
         name: 'Eggs',
         isVegetarian: true,
         isGlutenFree: true,
+        isOrganic: true,
         price: 6.99
     },
     {
         name: 'Flour',
         isVegetarian: true,
         isGlutenFree: false,
+        isOrganic: true,
         price: 5.99
     },
     {
         name: 'Chicken Breast',
         isVegetarian: false,
         isGlutenFree: true,
+        isOrganic: true,
         price: 8.99
     },
     {
         name: 'Ground Beef',
         isVegetarian: false,
         isGlutenFree: true,
+        isOrganic: true,
         price: 10.99
     },
     {
         name: 'Salmon',
         isVegetarian: false,
         isGlutenFree: true,
+        isOrganic: true,
         price: 9.99
     },
     {
         name: 'Noodles',
         isVegetarian: true,
         isGlutenFree: false,
+        isOrganic: false,
         price: 2.99
     }
 ]
 
 /**
- * Function that restricts products in grocery item view. Code borrowed from Caroline Barriere
+ * Function that restricts products in grocery item view. 
  * @param {*} prod List of products in inventory
- * @param {*} restrictions List of restrictions detailed by user
+ * @param {*} restriction List of restrictions detailed by user
  */
-function restrictProducts(prod, restrictions) {
+function restrictProducts(prod, restriction) {
     let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
-		}
+
+	for (let i=0; i<prod.length; i+=1) {
+
+        if(prod[i].isVegetarian && prod[i].isGlutenFree && prod[i].isOrganic && restriction.includes('vegetarian') && restriction.includes('glutenFree') && restriction.includes('organic')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isVegetarian && prod[i].isGlutenFree && restriction.includes('vegetarian') && restriction.includes('glutenFree')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isVegetarian && prod[i].isOrganic && restriction.includes('vegetarian') && restriction.includes('organic')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isOrganic && prod[i].isGlutenFree && restriction.includes('glutenFree') && restriction.includes('organic')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isVegetarian && restriction.includes('vegetarian')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isGlutenFree && restriction.includes('glutenFree')) {
+            product_names.push(prod[i]);
+        } else if(prod[i].isOrganic && restriction.includes('organic')) {
+            product_names.push(prod[i]);
+        }
 	}
 	return product_names;
 }
