26c1070ed6fbcf97b3991612781c23d02de62694
YasinUottawa <yasinelmi100@gmail.com>
2024-02-02 11:21:51 -0500

    fixed button and you can hide/show categories of food now

--- SEG3125-Lab3/scripts/main.js
+++ SEG3125-Lab3/scripts/main.js
@@ -136,3 +136,13 @@ function selectedItems(){
 		
 }
 
+function hide(elem){
+	div = document.getElementById(elem);
+	if(div.style.display == 'none'){
+		div.style.display = 'block';
+	}
+	else{
+		div.style.display = 'none';
+	}
+	
+}
