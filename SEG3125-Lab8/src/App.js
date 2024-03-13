b0e02bb9fdf5da7c8869c002961215fad1372152
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2024-03-12 23:26:28 -0400

    yurrr

--- SEG3125-Lab8/src/App.js
+++ SEG3125-Lab8/src/App.js
@@ -1,38 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="#">CanvaClique</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/gallery">Gallery</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">Communities</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/profile">Profile</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <header className="jumbotron jumbotron-fluid text-center">
-        <div className="container">
-          <h1 className="display-4">Welcome to CanvaClique</h1>
-          <p className="lead">Connect, share, and discover the world of art like never before.</p>
-        </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
