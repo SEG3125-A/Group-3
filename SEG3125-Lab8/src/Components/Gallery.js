1356fa3484d557548c5d0f82e97601da68cfd6e9
YasinUottawa <yasinelmi100@gmail.com>
2024-03-17 21:54:38 -0400

    added settings and gallery

--- SEG3125-Lab8/src/Components/Gallery.js
+++ SEG3125-Lab8/src/Components/Gallery.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import hideo from '../images/hideo.jpg';
+import heart from '../images/heart.png';
+import comment from '../images/comment.png';
+import yuto from '../images/yuto.jpg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Gallery(){
-	return (<h1>Gallery Page</h1>);
-}
+  return (
+    <div className='mx-auto'>
+      <h1>Gallery</h1>
+      <h2>Featured Posts</h2>
+      <div className="overflow-content mx-auto">
+        <div class="force-overflow mx-auto">
+          <Container>
+            <div className='card-container'>
+              <Card className='card'>
+                <Card.Header className ="bg-success text-white">Hideo_Kojima</Card.Header>
+                <Card.Img variant='top' src={hideo}/>
+                <Card.Body>
+                  <Container>
+                    <Row lg = {3}>
+                      <Col>
+                        <Image src={heart} fluid width="25%"/> 1,000,000
+                      </Col>
+                      <Col>
+                        <Image src={comment} fluid width="25%"/> 10,000
+                      </Col>
+                    </Row>
+                    <Card.Text>Hideo_Kojima: art....</Card.Text>
+                  </Container>
+                </Card.Body>
+              </Card>
 
+              {/* Card 2 */}
+              <Card className='card'>
+                <Card.Header className ="bg-success text-white">YutoSuzuki23</Card.Header>
+                <Card.Img variant='top' src={yuto}/>
+                <Card.Body>
+                  <Container>
+                    <Row lg= {3}>
+                      <Col>
+                        <Image src={heart} fluid width="25%"/> 1,000,000
+                      </Col>
+                      <Col>
+                        <Image src={comment} fluid width="25%"/> 10,000
+                      </Col>
+                    </Row>
+                    <Card.Text>YutoSuzuki23: Old Man Takamura... some spoilers for the next chapter!</Card.Text>
+                  </Container>
+                </Card.Body>
+              </Card>
+            </div>
+          </Container>
+          <br></br>
+          <p>pick up a pen</p>
+        </div>
+      </div>
+    </div>  
+  );
+}
 
 export default Gallery;
--- SEG3125-Lab8/src/Components/Settings.js
+++ SEG3125-Lab8/src/Components/Settings.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const Settings = () => <h1>Settings Page</h1>
+function Settings({ onLogout }) {
+	// Placeholder for a logout function. You should replace it with your actual logout logic
+	const handleLogout = () => {
+	  // Implement your logout logic here
+	  // This could involve clearing local storage, updating React context or Redux store, and redirecting the user
+	  console.log('Logging out...');
+	  //onLogout(); // Call the passed logout handler, if any
+	};
+  
+	return (
+	  <div className="settings-container">
+		<h2>Settings</h2>
+		<ul className="settings-list">
+		  {/* Other settings options can be added here */}
+		  <li className="settings-item">
+			<button onClick={handleLogout} className="logout-button">Log Out</button>
+		  </li>
+		</ul>
+	  </div>
+	);
+  }
 
 export default Settings;
